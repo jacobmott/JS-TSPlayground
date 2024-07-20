a25060dab7261d4ecde500327c849eec87b40c99
jacobmott <jacobmott@gmail.com>
2024-07-20 14:14:06 -0500

    Experimented with memory profiling in the browser
    Using the debugger keyword to trigger pausing/scope output
    
    Creating snapshots of memory usage in the debugger tools in chrome

---
+++ javascript/scope_and_debugger_and_memory_profiling.js
@@ -0,0 +1,46 @@
+//https://www.youtube.com/watch?v=SQP9VV5b9IM&t=1s
+//https://www.youtube.com/watch?v=WqNqeMjd28I
+
+//let throwAwayVariableToPrint2 =
+//  "using debugger keyword/feature here to cause the browser" +
+//  " to pause and allow you to inspect the variables in the current scope" +
+//  " we do this here so that we can take a diff between heap data" +
+//  " before and after this debug/snapshot";
+//console.log(throwAwayVariableToPrint2);
+//debugger;
+//
+//const scriptScopeVariable1 = "scriptScopeVariable1";
+//
+//function func1() {
+//  const localScopeVariable1 = "localScopeVariable1";
+//  console.log(localScopeVariable1);
+//  if (true) {
+//    const blockScopeVariable1 = "blockScopeVariable1";
+//    console.log(blockScopeVariable1);
+//    let throwAwayVariableToPrint =
+//      "using debugger keyword/feature here to cause the browser" +
+//      " to pause and allow you to inspect the variables in the current scope";
+//    console.log(throwAwayVariableToPrint);
+//    //debugger;
+//  }
+//}
+//
+//func1();
+//console.log(scriptScopeVariable1);
+
+/* 
+document.addEventListener("click", (e) => {
+  const a = `${e.clientX}, ${e.clientY}`;
+  console.log(a);
+}); */
+
+//create a map that grows in size/added to every millisecond
+
+/* const map = new Map();
+let i = 0;
+
+setInterval(() => {
+  map.set(i, `item ${i}`);
+  i++;
+  //console.log(`i ${i}`);
+}, 1); */
